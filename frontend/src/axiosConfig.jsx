53a2233ea31dc3ce6b505504ee352a41d2f90a17
bhavesh <agrawalbhavesh80@gmail.com>
2025-01-23 17:42:56 +0530

    w

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL:"https://connect-karo.onrender.com" || "http://localhost:3000",
+    withCredentials:true
 })
 
 export default axiosInstance
